9741e66d3c4d8d0cf5b3e8570d4348fd4fee2c65
Cray Elliott <MP2E@archlinux.us>
2014-10-26 02:18:11 -0700

    chocolateDoomMaster was broken, whoops!

--- chocolate-doom/master.nix
+++ chocolate-doom/master.nix
@@ -1,21 +1,24 @@
-{ stdenv, autoconf, fetchgit, pkgconfig, SDL, SDL_mixer, SDL_net }:
+{ stdenv, autoconf, automake, pkgconfig, SDL, SDL_mixer, SDL_net, fetchgit }:
 
 stdenv.mkDerivation rec {
   name = "chocolate-doom-20141026";
   src = fetchgit {
-    url = https://github.com/chocolate-doom/chocolate-doom;
+    url = "https://github.com/fragglet/chocolate-doom.git";
     rev = "f55a88fec2949c834aa6c4434f22fcaf717ee11d";
     sha256 = "08a35ar4nx88wrd8k2m4ycgis171m59sf2hn42f20cl10fwd6zr2";
   };
-
-  buildInputs = [ autoconf pkgconfig SDL SDL_mixer SDL_net ];
+  buildInputs = [ autoconf automake pkgconfig SDL SDL_mixer SDL_net ];
+  patchPhase = ''
+    sed -e 's#/games#/bin#g' -i src{,/setup}/Makefile.am
+    ./autogen.sh --prefix=$out
+  '';
 
   enableParallelBuilding = true;
 
   meta = {
     homepage = http://chocolate-doom.org/;
-    description = "A port that aims to recreate Doom the way it was experienced in the 90s";
-    license = stdenv.lib.licenses.gpl2;
+    description = "A Doom source port that accurately reproduces the experience of Doom as it was played in the 1990s";
+    license = stdenv.lib.licenses.gpl2Plus;
     maintainers = with stdenv.lib.maintainers; [ MP2E ];
   };
 }
