f966302798a49cb94e92e854d40226b1a1835cef
Cray Elliott <MP2E@archlinux.us>
2015-02-01 03:58:34 -0800

    update dolphin, eternity, add hoogle, foldl, turtle to haskellEnv

--- eternity-engine/master.nix
+++ eternity-engine/master.nix
@@ -1,11 +1,11 @@
 { stdenv, cmake, mesa, SDL, SDL_mixer, SDL_net, fetchgit }:
 
 stdenv.mkDerivation rec {
-  name = "eternity-engine-20141012";
+  name = "eternity-engine-20150131";
   src = fetchgit {
     url = git://github.com/team-eternity/eternity.git;
-    rev = "db21379abb33f6d92dc0e1665e527e4d49acc722";
-    sha256 = "0k54yvxqxnd60az21b734ka3myxkqb7pjmdp3klrkfwp1kl02ysc";
+    rev = "b653b6471f823c56a6f5297a7ce1e7560652deb7";
+    sha256 = "15yldd9w4ydl0vwv51lbglxgav2zg6m3l7pnmyl1ppzrijj0injy";
   };
 
   cmakeFlags = ''
