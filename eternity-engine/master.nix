aa30130bd2df376abbdc0f6d99f888f8bc908956
Cray Elliott <MP2E@archlinux.us>
2017-06-14 00:44:48 -0700

    update eternity engine to line up with upstream

--- eternity-engine/master.nix
+++ eternity-engine/master.nix
@@ -1,25 +1,27 @@
-{ stdenv, cmake, mesa, SDL, SDL_mixer, SDL_net, fetchFromGitHub, makeWrapper }:
+{ stdenv, cmake, mesa_noglu, SDL, SDL_mixer, SDL_net, makeWrapper }:
 
 stdenv.mkDerivation rec {
-  name = "eternity-engine-20170408";
+  name = "eternity-engine-${version}";
+  version = "20170613";
   src = ~/eternitygit;
 
   nativeBuildInputs = [ cmake makeWrapper ];
-  buildInputs = [ mesa SDL SDL_mixer SDL_net ];
+  buildInputs = [ mesa_noglu SDL SDL_mixer SDL_net ];
 
   enableParallelBuilding = true;
 
   installPhase = ''
-  mkdir -p $out/bin $out/lib
-  cp source/eternity $out/lib
-  cp -r base/ $out/lib
-  makeWrapper $out/lib/eternity $out/bin/eternity
+    install -Dm755 source/eternity $out/lib/eternity/eternity
+    cp -r $src/base $out/lib/eternity/base
+    mkdir $out/bin
+    makeWrapper $out/lib/eternity/eternity $out/bin/eternity
   '';
 
   meta = {
     homepage = http://doomworld.com/eternity;
     description = "New school Doom port by James Haley";
     license = stdenv.lib.licenses.gpl3;
+    platforms = stdenv.lib.platforms.linux;
     maintainers = with stdenv.lib.maintainers; [ MP2E ];
   };
 }
