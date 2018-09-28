6240ac0877f0011bbcd2adf8c6956db4e9d7f587
Cray Elliott <MP2E@archlinux.us>
2018-09-28 10:45:32 -0700

    bump GHC to 8.6.1, add a ton of overrides

--- haskell-projects/xmonad-contrib/default.nix
+++ haskell-projects/xmonad-contrib/default.nix
@@ -7,7 +7,6 @@ mkDerivation {
   pname = "xmonad-contrib";
   version = "0.14";
   src = lib.cleanSource /home/cray/XMonadContrib;
-  jailbreak = true;
   libraryHaskellDepends = [
     base bytestring containers directory extensible-exceptions filepath
     mtl old-locale old-time process random semigroups unix utf8-string
