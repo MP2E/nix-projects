c7e85e0f0e8f1036f48a007d69c888e4ffa259ef
Cray Elliott <MP2E@archlinux.us>
2018-09-28 21:53:48 -0700

    oos-randomizer: 2.1.1 -> 2.1.2

--- oos-randomizer/default.nix
+++ oos-randomizer/default.nix
@@ -3,13 +3,13 @@
 buildGoPackage rec {
   name = "oos-randomizer-${version}";
 
-  version = "2.1.1";
+  version = "2.1.2";
 
   src = fetchFromGitHub {
     owner = "jangler";
     repo = "oos-randomizer";
     rev = "${version}";
-    sha256 = "1i1cyw2ffgbqax12l3k2dmgiw8r3ar19rc915gz3chy0sya12hrv";
+    sha256 = "0r1p8zvr3776q73pdkkznpjads77x66c6adrslz8fd37iz3ipvah";
   };
 
   goPackagePath = "github.com/jangler/oos-randomizer";
